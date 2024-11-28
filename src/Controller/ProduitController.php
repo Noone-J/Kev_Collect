c70fb81c6b603cfd8d294d7708b77068d81a8f15
Noone-J <njoseph.ledantec2@gmail.com>
2024-11-28 09:32:27 +0100

    Test 1 vue produit

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,21 @@
+namespace App\Controller;
+
+use App\Entity\Produit;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produits', name: 'produit_list')]
+    public function list(): Response
+    {
+        // Récupérer tous les produits depuis le repository de l'entité Produit
+        $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+
+        // Renvoyer les produits à la vue Twig
+        return $this->render('produit/list.html.twig', [
+            'produits' => $produits,
+        ]);
+    }
+}
