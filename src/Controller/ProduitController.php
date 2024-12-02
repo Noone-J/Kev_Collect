b88cd619d130416030dc8a7c64a367586e605f44
Noone-J <njoseph.ledantec2@gmail.com>
2024-12-02 14:22:24 +0100

    /api/getProduit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -9,15 +9,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produits', name: 'produit_list')]
-    public function list(): Response
-    {
-        // Récupérer tous les produits depuis le repository de l'entité Produit
-        $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+    // #[Route('/produits', name: 'produit_list')]
+    // public function list(): Response
+    // {
+    //     // Récupérer tous les produits depuis le repository de l'entité Produit
+    //     $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
 
-        // Renvoyer les produits à la vue Twig
-        return $this->render('produit/list.html.twig', [
-            'produits' => $produits,
-        ]);
-    }
+    //     // Renvoyer les produits à la vue Twig
+    //     return $this->render('produit/list.html.twig', [
+    //         'produits' => $produits,
+    //     ]);
+    // }
 }
