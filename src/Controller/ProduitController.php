9fb646f3d7ee89c92f2e5ff2eefbdeead5120911
Noone-J <njoseph.ledantec2@gmail.com>
2024-12-03 14:01:27 +0100

    CreerProduit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,21 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class ProduitController extends AbstractController
 {
-    // #[Route('/produits', name: 'produit_list')]
-    // public function list(): Response
-    // {
-    //     // Récupérer tous les produits depuis le repository de l'entité Produit
-    //     $produits = $this->getDoctrine()->getRepository(Produit::class)->findAll();
+    #[Route('/produit', name: 'app_produit')]
+    public function list(): Response
+    {
+        return $this->render('produit/index.html.twig', [
+            'controller_name' => 'ProduitController',
+        ]);
+    }
+    
+    #[Route('/produit/CreerProduit', name: 'app_produit_creer_produit')]
+    public function CreerProduit(EntityManagerInterface $entityManager): Response
+    {
 
-    //     // Renvoyer les produits à la vue Twig
-    //     return $this->render('produit/list.html.twig', [
-    //         'produits' => $produits,
-    //     ]);
-    // }
+        $produit1 = new Produit();
+        $produit1->setLibeller("Coca");
+        $produit1->setDescription("Soda");
+        $produit1->setPrixUnitaire("2");
+        $entityManager->persist($produit1);
+
+        $entityManager->flush();
+
+        return new Response("ok"); 
+    }
 }
